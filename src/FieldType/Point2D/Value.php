72b9bd3c89ea3cea8498995c81e9cffb60a35083
Magda <magdalena.zuba@ez.no>
2019-12-13 14:24:53 +0100

    Step 7 - Add basic validation

--- src/FieldType/Point2D/Value.php
+++ src/FieldType/Point2D/Value.php
@@ -10,13 +10,24 @@ declare(strict_types=1);
 namespace App\FieldType\Point2D;
 
 use eZ\Publish\SPI\FieldType\Value as ValueInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 final class Value implements ValueInterface
-{
-    /** @var float|null */
+
+    /**
+     * @var float|null
+     *
+     * @Assert\NotBlank()
+     */
     private $x;
-    /** @var float|null */
+
+    /**
+     * @var float|null
+     *
+     * @Assert\NotBlank()
+     */
     private $y;
+
     public function __construct(?float $x = null, ?float $y = null)
     {
         $this->x = $x;
@@ -27,18 +38,22 @@ final class Value implements ValueInterface
     {
         return $this->x;
     }
+
     public function setX(?float $x): void
     {
         $this->x = $x;
     }
+
     public function getY(): ?float
     {
         return $this->y;
     }
+
     public function setY(?float $y): void
     {
         $this->y = $y;
     }
+
     public function __toString()
     {
         return "({$this->x}, {$this->y})";
